ca085b2d03946728e24ec85dadc455363647ecf8
Denys <go4denys@gmail.com>
2023-06-02 21:28:35 -0400

    add environment variables

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql';
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'hapi-server',
-    password: 'abc123',
-    database: 'buy-and-sell'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 export const db = {
